6a2d37b42f2f6b2ecc32c821366f4e834230c84d
cantikrahmi <rahmishofiyanti@gmail.com>
2026-04-23 23:20:22 +0700

    canmiee

--- Cantik Rahmi(S2)/BoardingQueue.java
+++ Cantik Rahmi(S2)/BoardingQueue.java
@@ -1,1 +1,42 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class BoardingQueue {
+    private Queue<String> queue = new LinkedList<>();
+
+    // Tambah penumpang
+    public void enqueue(String name) {
+        queue.add(name);
+        System.out.println(name + " masuk ke antrian.");
+    }
+
+    // Proses boarding
+    public void dequeue() {
+        if (queue.isEmpty()) {
+            System.out.println("Antrian kosong!");
+        } else {
+            System.out.println(queue.poll() + " sudah boarding.");
+        }
+    }
+
+    // Lihat depan
+    public void peek() {
+        if (queue.isEmpty()) {
+            System.out.println("Antrian kosong!");
+        } else {
+            System.out.println("Depan: " + queue.peek());
+        }
+    }
+
+    // Tampilkan semua
+    public void display() {
+        if (queue.isEmpty()) {
+            System.out.println("Antrian kosong!");
+        } else {
+            System.out.println("Isi antrian:");
+            for (String p : queue) {
+                System.out.println("- " + p);
+            }
+        }
+    }
+}
