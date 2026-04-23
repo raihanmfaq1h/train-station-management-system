795881a76bd3c30ecaab501deab0c82f855756bd
raihanfaqih <raihanmfaqih@gmail.com>
2026-04-23 20:26:03 +0700

    shared commits

--- shared/Ticket.java
+++ shared/Ticket.java
@@ -1,1 +1,31 @@
+public class Ticket {
+    private String passengerId;
+    private String trainId;
+    private String seat;
+    private int price;
+
+    public Ticket(String passengerId, String trainId, String seat, int price) {
+        this.passengerId = passengerId;
+        this.trainId = trainId;
+        this.seat = seat;
+        this.price = price;
+    }
+
+    public String getPassengerId() {return passengerId;}
+    public String getTrainId() {return trainId;}
+    public String getSeat() {return seat;}
+    public int getPrice() {return price;}
+
+
+    public void setPrice(int price) { this.price = price; }
+
+
+    @Override
+    public String toString() {
+        return "[Tiket] penumoang:  " + passengerId + 
+        " | kereta: " + trainId + 
+        " | kursi " + seat + 
+        " | Harga: Rp" + String.format("%,d", price); 
+}
+}
 
