450af157338c9d7e90fe2bb5ef40e93a9ee36f2a
cantikrahmi <rahmishofiyanti@gmail.com>
2026-04-24 14:52:20 +0700

    rahmi

--- shared/Passenger.java
+++ shared/Passenger.java
@@ -1,11 +1,23 @@
-public class Passenger{
+public class Passenger {
     private String id;
     private String name;
     private String ticketClass;
+
     public Passenger(String id, String name, String ticketClass) {
         this.id = id;
         this.name = name;
         this.ticketClass = ticketClass;
-    
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTicketClass() {
+        return ticketClass;
     }
 }
