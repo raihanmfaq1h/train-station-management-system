819e310708e03ef8336bb2ab20c56cb6c72dde6b
cantikrahmi <rahmishofiyanti@gmail.com>
2026-04-24 14:24:56 +0700

    rahmi

--- Cantik Rahmi(S2)/BoardingQueueDemo.java
+++ Cantik Rahmi(S2)/BoardingQueueDemo.java
@@ -4,21 +4,22 @@ public class BoardingQueueDemo {
         BoardingQueue queue = new BoardingQueue();
 
         queue.enqueue(new Passenger("PX01", "Rina Kartika", "Eksekutif"));
-        queue.enqueue(new Passenger("PX02","Dodi Prasetyo", "Bisnis"));
-        queue.enqueue(new Passenger("PX03","susi susilawati", "Ekonomi"));
+        queue.enqueue(new Passenger("PX02", "Dodi Prasetyo", "Bisnis"));
+        queue.enqueue(new Passenger("PX03", "Susi Susilawati", "Ekonomi"));
 
+        // tampilkan antrian
+        queue.displayQueue();
+
+        // lihat yang depan
+        System.out.println("Berikutnya: " + queue.peek().getName());
+
+        // proses boarding
+        queue.dequeue();
+        queue.dequeue();
+
+        queue.displayQueue();
+
+        queue.dequeue();
+        queue.dequeue(); // harus muncul error
     }
-    queue.displayQueue();
-
-    System.out.printIn(Berikutnya: " + queue.peek().getName());
-
-    queue.dequeue();
-    queue.dequeue();
-
-    queue.displayQueue();
-
-    queue.dequeue();
-    queue.dequeue(); // harus error
-}
-    
 }
