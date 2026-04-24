b811b69648dfba8af43fee2994ab662e87cae2a5
cantikrahmi <rahmishofiyanti@gmail.com>
2026-04-24 10:44:34 +0700

    canmieeee

--- Cantik Rahmi(S2)/BoardingQueue.java
+++ Cantik Rahmi(S2)/BoardingQueue.java
@@ -2,41 +2,54 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class BoardingQueue {
-    private Queue<String> queue = new LinkedList<>();
+    private Queue<String> queue;
 
-    // Tambah penumpang
-    public void enqueueI(String name) {
-        queue.add(name);
-        System.out.println(name + " masuk ke antrian.");
+    // Constructor
+    public BoardingQueue() {
+        queue = new LinkedList<>();
     }
 
-    // Proses boarding
+    // Tambah penumpang ke antrian
+    public void enqueue(String passengerName) {
+        queue.add(passengerName);
+        System.out.println(passengerName + " masuk ke antrian boarding.");
+    }
+
+    // Proses boarding (Keluar dari antrian)
     public void dequeue() {
         if (queue.isEmpty()) {
-            System.out.println("Antrian kosong!");
+            System.out.println("Antrian kosong tidak ada penumpang");
         } else {
-            System.out.println("Depan: " + queue.peek());
+            String passennger = queue.poll();
+            System.out.println(passennger + " sudah boarding.");
         }
     }
 
-    // Lihat depan
+    // Lihat  penumpang paling depan
     public void peek() {
+        if (queue.isEmpty()) {
+            System.out.println("Antrian kosong.");
+        } else {
+            System.out.println("Penumpang paling depan:" + queue.peek());
+        }
+    }
+
+    // Tampilkan semua antrian
+    public void displayQueue() {
         if (queue.isEmpty()) {
             System.out.println("Antrian kosong");
         } else {
-            System.out.println("Depan:" + queue.peek());
-        }
-    }
-
-    // Tampilkan semua
-    public void display() {
-        if (queue.isEmpty()) {
-            System.out.println("Antrian kosong!");
-        } else {
-            System.out.println("Isi antrian;");
-            for (String p : queue) {
-                System.out.println("- " + p);
+            System.out.println("Daftar antrian boarding:");
+            for (String passenger : queue) {
+                System.out.println("- " + passenger);
             }
         }
     }
-}
+
+
+
+           // Cek kosong atau tidak
+            public boolean isEmpty() {
+                return queue.isEmpty();
+                }
+            }
