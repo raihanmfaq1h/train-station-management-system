8e215b90ed2caef853612a41d33738253e5daea8
cantikrahmi <rahmishofiyanti@gmail.com>
2026-04-28 16:44:41 +0700

    iyacantik

--- alya/JourneyLogStackDemo.java
+++ alya/JourneyLogStackDemo.java
@@ -6,17 +6,26 @@ public class JourneyLogStackDemo {
 
         Stack.push(new train("T001", "Bandung", "Jakarta"));
         Stack.push(new train("T002", "Bogor", "Depok"));
-        Stack.push(new train("T003", "Cirebon", "Bekasi"));
+        Stack.push((new train("T003", "Cirebon", "Bekasi"));
 
         Stack.displayHistory();
 
         System.out.println("Terakhir: " + Stack.peek().getName());
-        
+
         Stack.pop();
         Stack.displayHistory();
-        
+
         Stack.pop();
         Stack.pop();
         Stack.pop(); // eror
     }
-}
+ }
+
+        
+
+        
+
+
+
+
+
